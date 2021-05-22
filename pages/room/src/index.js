1db09ad4999c46a4e791adb1b2b5acb74b9e5461
Felipe Augusto <felipe.augusto.rodrigues1@gmail.com>
2021-05-22 13:39:33 -0300

    feat: Create socket builder

---
+++ pages/room/src/index.js
@@ -0,0 +1,9 @@
+const room = {
+  id: Date.now(),
+  topic: 'JS Expert',
+}
+
+const user = {
+  img: 'https://cdn4.iconfinder.com/data/icons/famous-characters-add-on-vol-1-flat/48/Famous_Character_-_Add_On_1-22-256.png',
+  username: 'Batman',
+}
